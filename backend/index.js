4aa4748813ea1a8089d81a7cc12fc3c83083dda3
karthikeya890 <bunnymkk9@gmail.com>
2024-05-25 02:35:59 +0530

    sd

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const app = express();
 
 // app.use(compression());
-app.use(cors());
 app.use(
   cors({
     origin: ["https://dentee-frontend.vercel.app", "http://localhost:5173"],
